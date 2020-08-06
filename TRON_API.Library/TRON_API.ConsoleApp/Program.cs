c4ac9ec1412b072efc1426910ff8b8fdd2f605dc
ilya_ermoshin <ilya.ermoshin123@gmail.com>
2020-08-06 10:43:40 +0300

    add console application for example

---
+++ TRON_API.Library/TRON_API.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using TRON_API.Library;
+using TRON_API.Library.Requests.Accounts;
+
+namespace TRON_API.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tronConfig = new TronApiConfiguration("https://api.shasta.trongrid.io/", "https://api.shasta.trongrid.io/");
+            var accountsRequests = new AccountsRequests(tronConfig);
+            var getAccountInfo = await accountsRequests.GetAccount("TLFXzKWczwZJbrVz3QEsrnH2F4zb4BeFqh");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
